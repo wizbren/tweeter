a0d9ebc63440421b75116255836f10764c2369f0
Brendan Brown <brendan94brown@gmail.com>
2025-04-23 22:39:39 -0700

    Add createTweetElement function that builds and returns jQuery tweet element, and add timeSince function to record how long, in hours, since the tweet was made

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,3 +14,70 @@ $(document).ready(function() {   //Select tweet form, listen for submit event
       })
   });
 });
+
+function timeSince(timestamp) {
+  const seconds = Math.floor((Date.now() - timestamp) / 1000);
+  const hours = Math.floor(seconds / 3600);
+
+  if (hours < 1) {
+    return 'less than an hour ago';
+  }
+  if (hours === 1) {
+    return '1 hour ago';
+  }
+  return hours + ' hours ago';
+}
+
+/*Function to build and return tweets*/
+const createTweetElement = function(tweet) {
+  const howLongAgo = timeSince(tweet.created_at);
+
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div>
+          <img src="${tweet.user.avatars}" alt="User Avatar">
+          <span>${tweet.user.name}</span>
+        </div>
+        <span class="user-handle">${tweet.user.handle}</span>
+      </header>
+
+      <section class="tweet-text">
+        ${tweet.content.text}
+      </section>
+
+      <footer>
+        <span>${howLongAgo}</span>
+        <div>
+          <i class="fa-regular fa-flag"></i>
+          <i class="fa-regular fa-retweet"></i>
+          <i class="fa-regular fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+  return $tweet;      //Returns jQuery tweet element
+};
+
+
+
+
+
+/*// TEST / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.*/
