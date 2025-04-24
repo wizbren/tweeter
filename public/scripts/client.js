687a7ad3b609793724c0f4e3944ed8fdea2be891
Brendan Brown <brendan94brown@gmail.com>
2025-04-23 20:45:42 -0700

    Add event listener (using jQuery) for 'submit', and handler function to stop page from reloading on submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {   //Select tweet form, listen for submit event
+  
+  $('form').on('submit', function(event) {//Halts default browser behaviour of reloading
+    event.preventDefault();               //on form submission
 
+    console.log("Form submit prevented!");
+  });
+});
