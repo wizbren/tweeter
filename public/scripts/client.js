385819e4553d42c0cfc10a6f224f144ec051a102
Brendan Brown <brendan94brown@gmail.com>
2025-04-23 21:06:49 -0700

    Add AJAX POST request to send data to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {   //Select tweet form, listen for submit event
   
-  $('form').on('submit', function(event) {//Halts default browser behaviour of reloading
-    event.preventDefault();               //on form submission
+  $('form').on('submit', function(event) {  //Halts default browser behaviour of reloading
+    event.preventDefault();                 //on form submission
 
-    console.log("Form submit prevented!");
+    const serializedData = $(this).serialize();  //Serialize form data into query string
+
+    $.post('/api/tweets', serializedData)   //Send serialized data to server through POST req
+      .done(function(response) {
+        console.log('Success!', response);
+      })
+      .fail(function(error) {
+        console.error('Failed...', error);
+      })
   });
 });
